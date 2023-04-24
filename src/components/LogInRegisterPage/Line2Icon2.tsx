bc91b8857971348231dd87a2dab42b648547571f
BaitedAngel <angelo.arthur.castro61@aluno.ifce.edu.br>
2023-04-24 09:00:23 -0300

    delete EVERYTHING
    deleting EVERYTHING

--- src/components/LogInRegisterPage/Line2Icon2.tsx
+++
@@ -1,9 +0,0 @@
-import { memo, SVGProps } from 'react';
-
-const Line2Icon2 = (props: SVGProps<SVGSVGElement>) => (
-  <svg preserveAspectRatio='none' viewBox='0 0 492 4' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
-    <path d='M0 0L492 -5.44632e-08' stroke='#C4C5C4' strokeWidth={4} strokeLinecap='round' strokeDasharray='10 10' />
-  </svg>
-);
-const Memo = memo(Line2Icon2);
-export { Memo as Line2Icon2 };
