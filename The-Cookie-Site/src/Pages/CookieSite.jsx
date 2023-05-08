4ea80495a74380a2e006cbb98a9b75d43b54f042
BaitedAngel <angelo.arthur.castro61@aluno.ifce.edu.br>
2023-05-08 13:24:11 -0300

    Front-end Completo
    Design do front-end Completo sem problemas.

--- The-Cookie-Site/src/Pages/CookieSite.jsx
+++ The-Cookie-Site/src/Pages/CookieSite.jsx
@@ -1,7 +1,21 @@
 import '../main.css'
+import CookieCard from '../components/cookieCard.jsx';
 
 export default function CookieSite() {
     return (
-            <div>hello there</div>
+        <div className='bodyCookieSite'>
+            <div className='header'>
+                <div className='profile'>
+                    <img className='pfpPic' src='../public/img/sammy.jpg'/>
+                </div>
+                <div className='searchBar'>
+
+                    <input type='text' id='searchBar' placeholder='Qual cookie você quer buscar?'/>
+                </div>
+            </div>
+            <div className='CookieColumns'>
+                <CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard /><CookieCard />
+            </div>
+        </div>
     );
 }
---
+++ The-Cookie-Site/src/components/cookieCard.jsx
@@ -0,0 +1,14 @@
+import '../main.css'
+
+export default function cookieCard() {
+    return (
+            <div className='cookieCard'>
+                <div id='textBg'/>
+                <div id='text'>
+                    <h2 id='TitleCookie'>Titulo do Cookie</h2><br/>
+                    <p id='locationCookie'>Localização</p><br/>
+                    <p id='descriptionCookie'>Descrição</p>
+                </div>
+            </div>
+    );
+}
