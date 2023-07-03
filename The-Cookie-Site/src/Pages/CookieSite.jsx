900ad3c927e455625e5c7959c1bb1e4a10f02646
BaitedAngel <angelo.arthur.castro61@aluno.ifce.edu.br>
2023-07-02 23:19:08 -0300

    THERE we go

--- The-Cookie-Site/src/Pages/CookieSite.jsx
+++ The-Cookie-Site/src/Pages/CookieSite.jsx
@@ -1,8 +1,58 @@
 import '../main.css'
 import CookieCard from '../components/cookieCard.jsx';
 import AuthDetails from '../components/AuthDetails';
+import { db } from '../firebase'
+import { uid } from 'uid';
+import { set, ref } from 'firebase/database';
+import { useState } from 'react';
 
 export default function CookieSite() {
+
+    const [nameCookie, setNameCookie] = useState('');
+    const [locationCookie, setLocationCookie] = useState('');
+    const [description, setDescription] = useState('');
+    const [link, setLink] = useState('');
+
+    const handleNameChange=(e) => {
+        setNameCookie(e.target.value)
+    }
+
+    const handleLocationChange=(e) => {
+        setLocationCookie(e.target.value)
+    }
+
+    const handleDescriptionChange=(e) => {
+        setDescription(e.target.value)
+    }
+
+    const handleLinkChange=(e) => {
+        setLink(e.target.value)
+    }
+
+    //read
+
+    //write
+    const writeToDatabase = () => {
+        const uuid = uid();
+        set(ref(db, `/${uuid}`), {
+            nameCookie,
+            locationCookie,
+            description,
+            link,
+            uuid,
+        });
+
+        setLink([]);
+        setNameCookie([]);
+        setLocationCookie([]);
+        setDescription([]);
+    };
+
+    //update
+
+
+    //delete
+
 
     return (
         <div className='bodyCookieSite'>
@@ -10,10 +60,13 @@ export default function CookieSite() {
                 <div className='profile'>
                     <AuthDetails/>
                 </div>
-                <div className='searchBar'>
-                    <img src='/img/Vector_search_icon.svg.png' id='searchIcon'/>
-                    <input type='text' id='searchBar' placeholder='Qual cookie você quer buscar?'/>
-                </div>
+            </div>
+            <div className='cookiePuter'>
+                <input type="text" value={nameCookie} onChange={handleNameChange} placeholder='Name'/>
+                <input type="text" value={locationCookie} onChange={handleLocationChange} placeholder='location'/>
+                <input type='text' value={description} onChange={handleDescriptionChange} placeholder='description'/>
+                <input type='text' value={link} onChange={handleLinkChange} placeholder='link'/>
+                <button onClick={writeToDatabase}>Submit</button>
             </div>
             <div className='CookieColumns'>
                 <CookieCard />
--- The-Cookie-Site/src/components/cookieCard.jsx
+++ The-Cookie-Site/src/components/cookieCard.jsx
@@ -1,14 +1,44 @@
 import '../main.css'
+import { db } from '../firebase'
+import { ref, onValue, remove } from 'firebase/database';
+import { useState, useEffect } from 'react';
 
 export default function cookieCard() {
+
+    const [tudo, setTudo] = useState([]);
+
+    //read
+    useEffect(() => {
+        onValue(ref(db), snapshot => {
+            setTudo([]);
+            const data = snapshot.val();
+            if (data !== null){
+                Object.values(data).map((tudo) => {
+                    setTudo(oldArray => [...oldArray, tudo])
+                })
+            }
+        })
+    }, [])
+
+    //delete
+    const handleDelete = (tudo) => {
+        remove(ref(db, `/${tudo.uuid}`));
+    }
+
     return (
+        <>
+        {tudo.map((tudo) => (
             <div className='cookieCard'>
                 <div id='textBg'/>
                 <div id='text'>
-                    <h2 id='TitleCookie'>Titulo do Cookie</h2><br/>
-                    <p id='locationCookie'>Localização</p><br/>
-                    <p id='descriptionCookie'>Descrição</p>
+                    <h2 id='TitleCookie'>{tudo.nameCookie}</h2><br/>
+                    <p id='locationCookie'>{tudo.locationCookie}</p><br/>
+                    <p id='descriptionCookie'>{tudo.description}</p><br/>
+                    <a id='linkCookie' href={tudo.link} >{tudo.nameCookie}</a>
+                    <button id='deleteBtn' onClick={() => handleDelete(tudo)}>delete</button>
                 </div>
             </div>
+        ))}
+        </>
     );
 }
